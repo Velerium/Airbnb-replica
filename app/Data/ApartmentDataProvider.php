8e1765d1c693c4cc4aa69f6c260daeca2b593399
Velerium <bondaniandrea@libero.it>
2021-10-05 20:21:53 +0200

    Seeders done (mostly :'])

--- app/Data/ApartmentDataProvider.php
+++ app/Data/ApartmentDataProvider.php
@@ -16,6 +16,12 @@ class ApartmentDataProvider
         'Splendido Loft',
         'Villa a Bari',
         'Monolocale',
+        'Pröfinghof Epflplua - Vacanze in fattoria',
+        'Grazioso fienile/terrazzo ristrutturato con vista libera',
+        'Der Boutique Design Bauernhof: ANNAS SCHEUNE',
+        'La Grange des Anges',
+        'Il fienile atipico nel cuore dei Pirenei',
+        'Casa Famiglia, Stelida',
         ],
 
         'description' => [
@@ -29,26 +35,33 @@ class ApartmentDataProvider
             'Il mio loft e uno spazio ricavato in palazzo del XXVI secolo nel cuore di Roma.',
             'Splendida villa con pietra a vista.',
             'Elegante monolocale nel pieno centro di Torino.',
+            'L\'appartamento Epflplua con ca. 50 m² si trova al piano terra del nostro fienile, è esposto a sud e dispone di un ampio balcone esposto a sud (ca. 10 m²), una camera da letto con letto matrimoniale e un kojie letto separato con letto matrimoniale che è molto popolare tra i bambini, angolo cottura completamente attrezzato, soggiorno con divano letto e TV e un ampio bagno.',
+            'Questo vecchio fienile completamente ristrutturato per la vostra vacanza a Tautavel, è ideale per una vacanza in famiglia con bambini. Composto da un soggiorno di 50m2 con camino per le miti serate invernali, una bella terrazza di 25m2 con plancha per un\'estate conviviale, 4 camere da letto, WC separato, bagno con vasca idromassaggio e doccia + doccia italiana. Numerosi comfort moderni. Alloggi completamente climatizzati e WIFI GRATUITO!!!',
+            'La porta dell\'Elztal nella Foresta Nera e della Glottertal, famosa in tutto il mondo, forma la piccola "città lenta" Waldkirch. Nel sobborgo di Buchholz si trova nel mezzo del centro storico ANNAS SCHEUNE. Una casa colonica con diversi annessi del 17° secolo, che è stata ristrutturata biologicamente nel 2016. Secolo. L\'arredamento con gli oggetti d\'antiquariato, i classici del design e le mobili su misura completa il moderno stile country del fienile.',
+            'Casa di charme tra mare e natura fienile ristrutturato, fascino dell\'antico con comfort moderni, situata al crocevia del Mediterraneo e delle Cevenne, tra Nimes, Montpellier, Ales e le spiagge. Piano terra superficie abitabile di 58 m2, soggiorno, SAM, cucina a vista, attrezzata con nuovo. Zona esterna, piscina di 17 m2, cucina estiva. Piano terra, letto matrimoniale (doccia italiana, doppio lavabo, spogliatoio, tv). WC, lavanderia. Piano 3 suite (doccia italiana, doppio lavabo, spogliatoio, tv). WC indipendente.',
+            'Il nostro grazioso fienile tipico di Cominac si trova a 780 metri sul livello del mare e offre una tranquillità e una calma favorevole al relax. Le passeggiate si trovano direttamente dal villaggio, a 20 minuti dai bagni Aulus Les Bains e a 35 minuti dalla stazione di Guzet Neige. I villaggi di Oust e Seix situati a circa 10/15 minuti dal fienile vi permetteranno di trovare tutti i comfort necessari.',
+            'Casa Famiglia è un grande condominio (98 m2) con 2 piani. Al piano principale si trova una camera da letto con letto matrimoniale e bagno, un soggiorno a pianta aperta con cucina completamente attrezzata e una terrazza con una splendida vista sui letti del giardino e sulla zona piscina. Al piano seminterrato ci sono due camere da letto, una camera da letto ha 1 letto matrimoniale e l\'altra ha 2 letti matrimoniali e un bagno e zona lavanderia.',
+
         ],
 
         'rooms_n' => [
-            4, 11, 7, 5, 3, 1, 3, 3, 5, 3,
+            4, 11, 7, 5, 3, 1, 3, 3, 5, 3, 5, 7, 4, 10, 5, 9,
         ],
 
         'beds_n' => [
-            3, 9, 6, 4, 1, 2, 2, 3, 3, 2,
+            3, 9, 6, 4, 1, 2, 2, 3, 3, 2, 2, 6, 2, 8, 3, 4,
         ],
 
         'bathrooms_n' => [
-            1, 2, 4, 2, 1, 1, 1, 2, 3, 1, 
+            1, 2, 4, 2, 1, 1, 1, 2, 3, 1, 1, 2, 1, 4, 1, 2,
         ],
 
         'guests_n' =>[
-            2, 9, 10, 4, 4, 2, 2, 3, 5, 2,
+            2, 9, 10, 4, 4, 2, 2, 3, 5, 2, 6, 8, 3, 8, 6, 8,
         ],
 
         'square_meters' => [
-            50, 410, 290, 32, 70, 50, 120, 100, 230, 110,
+            50, 410, 290, 32, 70, 50, 120, 100, 230, 110, 50, 175, 125, 250, 96, 196,
         ],
 
         'address' => [
@@ -62,6 +75,12 @@ class ApartmentDataProvider
             'Via Margutta 33, Roma (RM)',
             'Via Nicolò Piccini 28, Bari (BA)',
             'Via Po 95, Torino (TO)', 
+            'Via Eugenio Ruspoli 72, Genova (GE)',
+            'Via Claudio Monteverdi 24, Firenze (FI)',
+            'Via Ernesto Simini 32, Lecce (LE)',
+            'Via dell\' Uva 12, Trapani (TP)',
+            'Via Trilussa 2, Pescara (PE)',
+            'Via Gallinara 1, Cagliari(CA)',
         ],
 
         'image' => [
@@ -116,20 +135,73 @@ class ApartmentDataProvider
             10 => [
                   'https://a0.muscache.com/im/pictures/3f4db92f-c393-484a-9b4d-7a3816fee26a.jpg',
             ],
+            11 => [
+                'https://a0.muscache.com/im/pictures/567fcd06-5d40-407c-a66f-cc240bb97435.jpg',
+                'https://a0.muscache.com/im/pictures/e3157d91-69e9-4ac1-992f-388ef4d11221.jpg',
+                'https://a0.muscache.com/im/pictures/miso/Hosting-45116484/original/c6183f41-6c9a-4a10-82fc-9454fd3b3f98.jpeg',
+                'https://a0.muscache.com/im/pictures/miso/Hosting-45116484/original/7057668b-d819-4fb8-b741-574eb74ee644.jpeg',
+                'https://a0.muscache.com/im/pictures/miso/Hosting-45116484/original/02d9d5bc-71cf-4700-ab40-a090f0d8f111.jpeg',
+                'https://a0.muscache.com/im/pictures/miso/Hosting-45116484/original/3a923435-4f2a-4820-b60f-59e46cc06af7.jpeg',
+                'https://a0.muscache.com/im/pictures/13388ca8-4e9d-45fc-9e58-9d46bc110c52.jpg'
+            ],
+            12 => [
+                'https://a0.muscache.com/im/pictures/07eac1e1-daea-4476-85a3-75c0dd94a448.jpg',
+                'https://a0.muscache.com/im/pictures/miso/Hosting-44005370/original/f9522752-e201-41c4-a00c-def8c57b1470.jpeg',
+                'https://a0.muscache.com/im/pictures/097aa9ce-5127-4c93-bd26-2f838180c234.jpg',
+                'https://a0.muscache.com/im/pictures/1ec358f4-3959-46b0-8d50-a678d29cc106.jpg',
+                'https://a0.muscache.com/im/pictures/9f0bfa27-7b26-4aac-a456-dd6381494102.jpg'
+            ],
+            13 => [
+                'https://a0.muscache.com/im/pictures/5a84dd08-a2e8-4721-9149-523488bed095.jpg',
+                'https://a0.muscache.com/im/pictures/fca4e2b1-90a8-4671-a7ec-da0958c1c243.jpg',
+                'https://a0.muscache.com/im/pictures/85bc5fbc-466f-4b00-9a53-46e6b016e4b7.jpg',
+                'https://a0.muscache.com/im/pictures/7a069380-8d31-4ba2-b8ed-c4b8d05f5c78.jpg',
+                'https://a0.muscache.com/im/pictures/56b84584-f30f-468b-a4a1-f1e2eab719b8.jpg',
+                'https://a0.muscache.com/im/pictures/a0f8affd-30f3-4c8d-b2d7-327b187aa088.jpg'
+            ],
+            14 => [
+                'https://a0.muscache.com/im/pictures/pro_photo_tool/Hosting-22681968-unapproved/original/10e0b5fb-a087-478e-9a26-891d15ca0737.JPEG',
+                'https://a0.muscache.com/im/pictures/c06cb614-b288-45b7-8434-ddab5fb02189.jpg',
+                'https://a0.muscache.com/im/pictures/pro_photo_tool/Hosting-22681968-unapproved/original/7ed1ddda-fa5b-4673-bbd4-cdf2f00d551f.JPEG',
+                'https://a0.muscache.com/im/pictures/pro_photo_tool/Hosting-22681968-unapproved/original/3b107f4d-d8aa-471a-9f4d-b3b58e71a32f.JPEG',
+                'https://a0.muscache.com/im/pictures/pro_photo_tool/Hosting-22681968-unapproved/original/70072bed-7329-4d37-9569-c1cf3b3f58ed.JPEG',
+                'https://a0.muscache.com/im/pictures/12cce3d4-b743-4155-9378-17271884e933.jpg'
+            ],
+            15 => [
+                'https://a0.muscache.com/im/pictures/767b2022-3677-441d-bab8-b459986ee36e.jpg',
+                'https://a0.muscache.com/im/pictures/96f6287c-552a-44a2-879c-42de7be846b9.jpg',
+                'https://a0.muscache.com/im/pictures/2a5e2d52-75c4-42ff-be1b-6fe111f8a332.jpg',
+                'https://a0.muscache.com/im/pictures/954e6011-85ef-42de-a6a2-ee2860ebca99.jpg',
+                'https://a0.muscache.com/im/pictures/873e0261-a73e-4115-8519-233370672026.jpg',
+                'https://a0.muscache.com/im/pictures/b80fd3e2-8754-4022-825b-94f549c49a19.jpg',
+                'https://a0.muscache.com/im/pictures/31dc7836-4137-46d1-8c3d-7f3ce6a0c23c.jpg'
+            ],
+            16 => [
+                'https://a0.muscache.com/im/pictures/2a519efa-abf7-406f-9852-ace4b6617fde.jpg',
+                'https://a0.muscache.com/im/pictures/7c8ddff6-a827-4d36-98a1-f235d355c024.jpg',
+                'https://a0.muscache.com/im/pictures/e4708b4c-cc82-4ff5-986b-4c0326f4c0cf.jpg',
+                'https://a0.muscache.com/im/pictures/8856b382-af06-4899-9a95-1ff1c8b09d5e.jpg',
+                'https://a0.muscache.com/im/pictures/74f27f0b-9c0d-4375-84b2-988f184688ba.jpg',
+                'https://a0.muscache.com/im/pictures/bef436f4-407f-49fe-aa2b-c1bbb5a2d9a0.jpg',
+                'https://a0.muscache.com/im/pictures/f9f96d54-b55b-42a6-b219-2e45c24bf441.jpg'
+            ],
         ],
 
         'latitude' => [
-            43.7318, 45.9719, 44.8652, 45.4704, 44.1707, 43.7323, 45.4698, 41.9089, 41.1255, 45.0663, 
+            43.7318, 45.9719, 44.8652, 45.4704, 44.1707, 43.7323, 45.4698, 41.9089, 41.1255, 45.0663, 46.6513,
+            46.7377, 46.5206, 45.1721, 46.8759, 39.1964,
         ],
 
         'longitude' => [
-            11.2104, 7.9572, 8.9189, 10.7741, 8.3505, 11.2165, 9.1968, 12.4792, 16.8706, 7.6935
+            11.2104, 7.9572, 8.9189, 10.7741, 8.3505, 11.2165, 9.1968, 12.4792, 16.8706, 7.6935, 11.1649,
+            10.8485, 11.3553, 7.9060, 11.4858, 9.1521,
         ],
 
         'visible' => true,
 
         'price' => [
-            390.00, 190.00, 199.00, 135.00, 158.00, 240.00, 350.00, 410.00, 550.00, 220.00,
+            390.00, 190.00, 199.00, 135.00, 158.00, 240.00, 350.00, 410.00, 550.00, 220.00, 193.00,
+            112.00, 191.00, 287.00, 140.00, 236.00,
         ],
     ];
 
